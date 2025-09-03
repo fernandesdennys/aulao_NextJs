0f2a900a42ca735139b072ad4c29b7fe71374eeb
Dennys Fernandes <dennys.projetos@outlook.com>
2025-09-03 17:03:17 -0300

    Add dynamic and static navigation pages
    Introduces a dynamic cliente page using Next.js routing and a static navegação page. Updates the index page to include navigation links to these new pages. Also refines layout and app styles for improved appearance and removes commented code from Layout.module.css.

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -5,18 +5,20 @@ import Navegador from './../components/Navegador';
 
 export default function Inicio() {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexWrap: 'wrap',
-      height: '100vh'
-    }}>
-        <Navegador texto="estilo" destino="estilo"/>
-        <Navegador texto="exHeader" destino="exHeader" cor="darkgoldenrod"/>
-        <Navegador texto="jsx" destino="jsx" cor="crimson"/>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+      }}
+    >
+      {<Navegador texto="estilo" destino="estilo" />}
+      <Navegador texto="exHeader" destino="exHeader" cor="darkgoldenrod" />
+      <Navegador texto="jsx" destino="jsx" cor="crimson" /><Navegador texto="Navegação #01" destino="/navegacao" cor="mediumspringgreen"/>
+      <Navegador texto="Navegação #02" destino="/cliente/sp-2/984576" cor="blue" />
     </div>
-  )
+  );
 }
 /* export default function Inicio() {
   return (
